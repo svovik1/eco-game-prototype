fe9e23561fe711ab39ed6924d2d8401c708cdc12
Dmytro Kovalchuk <dimasmith@gmail.com>
2013-12-06 23:42:51 +0200

    support translation of html entries #2

--- public_html/js/app/app.js
+++ public_html/js/app/app.js
@@ -9,7 +9,12 @@ $(document).ready(function() {
     var view = new View(game);
     
     game.start();
-    view.render();    
+    view.render();   
+    
+    $(".i18n").each(function(){
+        var i18nEl = $(this);
+        i18nEl.text(t(i18nEl.text()));
+    });
     
 });
 
--- public_html/js/app/i18n/ua.js
+++ public_html/js/app/i18n/ua.js
@@ -1,13 +1,19 @@
 (function(){
     translator.registerTranslations({
-        locale: "ru",
-        "Modernize Manufacturing": "Модернизировать производство",
-        "Use Atomic Energy": "Развивать атомную энергетику",
-        "Take Money and Get Out": "Забрать все деньги и сбежать из страны",
-        "Flood":"Наводнение",
-        "Water level increases. It flows over fields reducing food amount. Drinking water becomes polluted with mud. Money spent to resolve problems":"У вас началось наводнение. От грязной воды пострадали сельскохозайственные угодья и резервуары питьевой воды.",
-        "You dioxide level is below ":"Уровень CO² опустился ниже отметки ",
-        "You have lack of some vital resources":"У вас закончились необходимые ресурсы"
+        locale: "uk",
+        "Modernize Manufacturing": "Модернізувати виробництво",
+        "Use Atomic Energy": "Розвивати атомну енергетику",
+        "Take Money and Get Out": "Забрати гроші і втекти з країни",
+        "Flood":"Повінь",
+        "Water level increases. It flows over fields reducing food amount. Drinking water becomes polluted with mud. Money spent to resolve problems":"У вас сталася повінь. Брудна вода затопила поля і прісні озера. Країна втратила багато їжі, питної води та грошей на подолання наслідків повені",
+        "You dioxide level is below ":"Рівень CO² менший ",
+        "You have lack of some vital resources":"У вас закінчилися необхідні ресурси",
+        "Available cards": "Доступні картки",
+        "Cards in use": "Вибрані картки",
+        "Complete move": "Завершити хід",
+        "Step: ": "№ Ходу: ",
+        "Game Over": "Гру закінчено",
+        "Restart Game": "Розпочати гру заново"
     });
 })();
 
