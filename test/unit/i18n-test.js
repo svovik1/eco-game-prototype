f32910d5f49347883e92b1021fa41b6e53f1261a
Dmytro Kovalchuk <dimasmith@gmail.com>
2013-12-08 12:22:37 +0200

    Some unit tests for translator

--- test/unit/i18n-test.js
+++ test/unit/i18n-test.js
@@ -2,7 +2,7 @@ define(["app/i18n"], function(translator) {
     describe("Translation", function() {
         var uk, ru;
         beforeEach(function() {
-            
+            translator.locale("uk");
             uk = {
                 locale: "uk",
                 "translated": "translation_uk"
@@ -18,7 +18,26 @@ define(["app/i18n"], function(translator) {
             var untranslatedKey = "not_translated";
             expect(translator.get(untranslatedKey)).toBe(untranslatedKey);
         });
-
+        
+        it("return translated value when translation is present", function(){
+           var translatedKey = "translated";
+           translator.registerTranslations(uk);
+           expect(translator.get(translatedKey)).toBe(uk[translatedKey]);
+        });
+        
+        it("returns values from correct locale", function(){
+            var translatedKey = "translated";
+           translator.registerTranslations(uk);
+           translator.registerTranslations(ru);
+           translator.locale("ru");
+           expect(translator.get(translatedKey)).toBe(ru[translatedKey]);           
+        });
+        
+        it("changes locale", function(){
+           expect(translator.locale()).toBe("uk"); 
+           translator.locale("ru");
+           expect(translator.locale()).toBe("ru"); 
+        });
 
     });
 
